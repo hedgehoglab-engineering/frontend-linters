fd850457aeea97e7f136be7dcf3a650dc5687c73
Sam Turrell <sam.turrell@netsells.co.uk>
2023-10-14 20:03:16 +0100

    Remove ignored files

--- packages/eslint-config-ts/dist/index.js
+++
@@ -1,6 +0,0 @@
-// src/index.ts
-var config = {};
-var src_default = config;
-export {
-  src_default as default
-};
--- packages/stylelint-config/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from 'stylelint';
-
-declare const config: Config;
-
-export { config as default };
-export = config
